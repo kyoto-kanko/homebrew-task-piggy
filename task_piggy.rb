1712108bd14bba44ad0daa832e4029e56e974533
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-03-19 01:02:05 +0900

    sha256変更

--- task_piggy.rb
+++ task_piggy.rb
@@ -2,7 +2,7 @@ class TaskPiggy < Formula
     desc "A simple task manager"
     homepage "https://github.com/kyoto-kanko/task-piggy"
     url "https://github.com/kyoto-kanko/task-piggy/releases/download/v0.1.0-beta/task-piggy.tar.gz"
-    sha256 "07e08e23a7a99aba7349f4bd3e877100c07dd27a8fc7882a22e325f6784a5db4"
+    sha256 "77665a61e1eee07f51873e319bff732975de104c887b51ab0cf09d49711066c9"
     version "0.1.0-beta"
     depends_on "ruby"
   
