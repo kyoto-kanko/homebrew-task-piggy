d874cf84fcef475f9cb1ab5626e6a4b0bb8fc037
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-03-20 23:42:44 +0900

    bundle install 追加

--- task_piggy.rb
+++ task_piggy.rb
@@ -3,16 +3,15 @@ class TaskPiggy < Formula
     homepage "https://github.com/kyoto-kanko/task-piggy"
     url "https://github.com/kyoto-kanko/task-piggy/releases/download/v0.1.0-beta/task-piggy.tar.gz"
     sha256 "77665a61e1eee07f51873e319bff732975de104c887b51ab0cf09d49711066c9"
-    version "0.1.0-beta"
     depends_on "ruby"
   
     def install
-      # シェバングはリポジトリ内のファイルに含まれるべき
+      system "gem", "install", "bundler"
+      system "bundle", "install"
       bin.install "app/main.rb" => "task-piggy"
     end
   
     test do
-      # 実際にコマンドが実行できるかの最小限のテスト
       system "#{bin}/task-piggy", "help"
     end
   end
