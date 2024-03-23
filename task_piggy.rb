52a4f9a88fb20d31976900ea0d369fcbcdd2b33d
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-03-23 23:59:39 +0900

    bundle install の記述更新

--- task_piggy.rb
+++ task_piggy.rb
@@ -7,7 +7,7 @@ class TaskPiggy < Formula
   
     def install
       system "gem", "install", "bundler"
-      system "bundle", "install"
+      system "bundle", "config", "set", "--local", "without", "production"
       bin.install "app/main.rb" => "task-piggy"
     end
   
